95852356751bde9f66bceff42ac8b74115273c8c
段文豪 <duanwenhao@shawya.com>
2023-04-18 11:08:54 +0800

    80订单模块修改为9001

--- consumer-order9001/src/main/java/cn/hao/cloud/ConsumerOrderApplication9001.java
+++ consumer-order9001/src/main/java/cn/hao/cloud/ConsumerOrderApplication9001.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ConsumerOrderApplication80 {
+public class ConsumerOrderApplication9001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerOrderApplication80.class, args);
+        SpringApplication.run(ConsumerOrderApplication9001.class, args);
     }
 }
