b0f57433c6af790934b1d14bca99f6bc75e804d1
段文豪 <duanwenhao@shawya.com>
2023-04-20 11:43:24 +0800

    逻辑遗漏

--- consumer-custom-load-balancer-order9004/src/main/java/cn/hao/cloud/config/WebMvcConfig.java
+++ consumer-custom-load-balancer-order9004/src/main/java/cn/hao/cloud/config/WebMvcConfig.java
@@ -4,9 +4,10 @@ package cn.hao.cloud.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate getRestTemplate() {
